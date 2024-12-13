587457f28d761e3bcce9a281e745d45a8eae0960
Lee_D <leegreat21@gmail.com>
2024-12-13 20:58:28 +0200

    models

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -1,6 +1,39 @@
 export const signup = async (req, res) => {
   try {
     const { fullName, username, password, confirmPassword, gender } = req.body;
+
+    if (password !== confirmPassword) {
+      return res.status(400).json({ message: "Passwords do not match" });
+    }
+    const user = await User.findOne({ username });
+
+    if (user) {
+      return res.status(400).json({ error: "username already exists" });
+    }
+    //HASH PASSWORD HERE
+
+    const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+    const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+
+    const newUser = new User({
+      fullName,
+      username,
+      password,
+      gender,
+      profilePic:
+        gender === "male" || gender === "other"
+          ? boyProfilePic
+          : girlProfilePic,
+    });
+
+    await newUser.save();
+
+    res.status(201).json({
+      _id: newUser._id,
+      fullName: newUser.fullName,
+      username: newUser.username,
+      profilePic: newUser.profile,
+    });
   } catch (error) {
     console.log(error);
   }
--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,21 @@
 import dotenv from "dotenv";
-dotenv.config();
-
 import express from "express";
+
 import authRoutes from "./routes/auth.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.send("Hey ya :-)");
-});
+dotenv.config();
+
+app.use(express.json()); // to parse incoming requests with JSON payloads
 
 app.use("/api/auth", authRoutes);
+
+// app.get("/", (req, res) => {
+//   res.send("Hey ya :-)");
+// });
 
 app.listen(PORT, () => {
   connectToMongoDB();
