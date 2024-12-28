f1e1c89de13b9ea8f02c20b992e1abcb66dd2141
Lee_D <leegreat21@gmail.com>
2024-12-28 18:30:36 +0200

    backend-completion

--- backend/controllers/message.controllers.js
+++ backend/controllers/message.controllers.js
@@ -1,5 +1,6 @@
 import Conversation from "./../models/conversation.model.js";
 import Message from "./../models/message.model.js";
+
 export const sendMessage = async (req, res) => {
   try {
     const { message } = req.body;
@@ -26,10 +27,30 @@ export const sendMessage = async (req, res) => {
       conversation.messages.push(newMessage._id);
     }
 
-    await conversation.save();
-    await newMessage.save();
+    // SOCKET IO FUNCTIONALITY TO BE ADDED HERE
+
+    // await conversation.save();
+    // await newMessage.save();
+
+    await Promise.all([conversation.save(), newMessage.save()]);
 
     res.status(201).json(newMessage);
+  } catch (error) {
+    console.log("Error in sendMessage controller: ", error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+export const getMessages = async (req, res) => {
+  try {
+    const { id: userToChatId } = req.params;
+    const senderId = req.user._id;
+
+    const conversation = await Conversation.findOne({
+      participants: { $all: [senderId, userToChatId] },
+    }).populate("messages");
+
+    res.status(200).json(conversation.messages);
   } catch (error) {
     console.log("Error in sendMessage controller: ", error.message);
     res.status(500).json({ error: "Internal server error" });
