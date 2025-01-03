c43b56a8b8ab190766e3b12a2ef0b82f5e297448
Lee_D <leegreat21@gmail.com>
2025-01-03 14:36:45 +0200

    daisy

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./index.html", 
-    "./src/**/*.{js,jsx,ts,tsx}",  // Ensure your React files are being included
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}", // Ensure your React files are being included
   ],
   theme: {
     extend: {},
   },
+  // eslint-disable-next-line no-undef
   plugins: [require("daisyui")],
 };
