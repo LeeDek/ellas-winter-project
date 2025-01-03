6cd55958cd20996aaf7075896a50f35f26c00189
Lee_D <leegreat21@gmail.com>
2025-01-03 13:39:56 +0200

    daisyUI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import './Index.css'
 
 function App() {
 
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+module.exports = {
+  content: [
+    "./index.html", 
+    "./src/**/*.{js,jsx,ts,tsx}",  // Ensure your React files are being included
+  ],
   theme: {
     extend: {},
   },
-  // eslint-disable-next-line no-undef
   plugins: [require("daisyui")],
 };
