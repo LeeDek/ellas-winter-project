2ca06be3605b04dc2a4638c7f274295dc1b90428
Lee_D <leegreat21@gmail.com>
2025-02-08 17:42:02 +0200

    search-engine

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -1,11 +1,13 @@
 /* eslint-disable react/prop-types */
 import { useAuthContext } from "../../context/AuthContext";
 import useConversations from "../../zustand/useConversation";
+import { extractTime } from "./../../../../backend/utilities/extractTime";
 
 const Message = ({ message }) => {
   const { authUser } = useAuthContext();
   const { selectedConversation } = useConversations();
   const fromMe = message.senderId === authUser._id;
+  const formattedTime = extractTime(message.createdAt);
   const chatClassName = fromMe ? "chat-end" : "chat-start";
   const profilePic = fromMe
     ? authUser.profilePic
@@ -19,11 +21,11 @@ const Message = ({ message }) => {
           <img alt="Tailwind CSS chat bubble component" src={profilePic} />
         </div>
       </div>
-      <div className={`chat-bubble text-white bg-blue-500 ${bubbleColor}`}>
+      <div className={`chat-bubble text-white ${bubbleColor}`}>
         {message.message}
       </div>
       <div className="chat-footer opacity-50 text-xs flex gap-1 items-center  text-gray-200">
-        12:42
+        {formattedTime}
       </div>
     </div>
   );
