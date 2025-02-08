c7df3f6492fc4d600d14c7aa8789d5522e23eae3
Lee_D <leegreat21@gmail.com>
2025-02-08 15:59:51 +0200

    render-messages

--- backend/controllers/message.controllers.js
+++ backend/controllers/message.controllers.js
@@ -50,9 +50,13 @@ export const getMessages = async (req, res) => {
       participants: { $all: [senderId, userToChatId] },
     }).populate("messages");
 
-    res.status(200).json(conversation.messages);
+    if (!conversation) return res.status(200).json([]);
+
+    const messages = conversation.messages;
+
+    res.status(200).json(messages);
   } catch (error) {
-    console.log("Error in sendMessage controller: ", error.message);
+    console.log("Error in getMessage controller: ", error.message);
     res.status(500).json({ error: "Internal server error" });
   }
 };
--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -7,10 +7,17 @@ const Messages = () => {
   console.log("Messages: ", messages);
   return (
     <div className="px-4 flex-1 overflow-auto">
-      {loading && [...Array(3)].map((_, idx) => <MessageSkeleton key={idx} />)}
+      {!loading &&
+        messages.length > 0 &&
+        messages.map((message) => (
+          <Message key={message._id} message={message} />
+        ))}
 
+      {loading && [...Array(3)].map((_, idx) => <MessageSkeleton key={idx} />)}
       {!loading && messages.length === 0 && (
-        <p className="text-center">Send a message to start the conversation</p>
+        <p className="text-center text-gray-400">
+          Send a message to start the conversation
+        </p>
       )}
     </div>
   );
